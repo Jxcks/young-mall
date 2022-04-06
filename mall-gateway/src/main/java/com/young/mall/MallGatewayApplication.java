2bbce5c6cd1630b77763f0b36a01157f1e75ef3a
Jxcks <1278803062@qq.com>
2022-04-06 19:03:31 +0800

    初始项目提交

---
+++ mall-gateway/src/main/java/com/young/mall/MallGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.young.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+    }
+}
